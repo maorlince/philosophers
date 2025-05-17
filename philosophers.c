f1089894d877c528a07dce16f1fb4813f6e977c8
Manon Lemercier <mlemerci@f1br3s7.clusters.42paris.fr>
2025-05-17 21:43:02 +0200

    first

--- philosophers.c
+++ philosophers.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   philosophers.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: manon <manon@student.42.fr>                +#+  +:+       +#+        */
+/*   By: mlemerci <mlemerci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/08 22:33:32 by manon             #+#    #+#             */
-/*   Updated: 2025/05/16 19:26:48 by manon            ###   ########.fr       */
+/*   Updated: 2025/05/17 21:39:53 by mlemerci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,19 @@
 
 int	ft_atoi(const char *str)
 {
-	int	signe;
+	int	sign;
 	int	res;
 	int	i;
 
 	i = 0;
 	res = 0;
-	signe = 1;
+	sign = 1;
 	while ((str[i] >= 9 && str[i] <= 13) || str[i] == ' ')
 		i++;
 	if (str[i] == '-' || str[i] == '+')
 	{
 		if (str[i] == '-')
-			signe = -1;
+			sign = -1;
 		i++;
 	}
 	while (str[i] >= '0' && str[i] <= '9')
@@ -36,38 +36,50 @@ int	ft_atoi(const char *str)
 		res = res * 10 + (str[i] - '0');
 		i++;
 	}
-	return (signe * res);
+	return (sign * res);
 }
 
 void live_likeem()
 {
-	ft_printf("eat\n");
 	pthread_mutex_lock (pthread_mutex_t * mutex);
-	ft_printf("thinks\n");
+	ft_printf("%d %s eat for the knowledges🍏\n", timestamp_in_m, philosopher[i]);
 	pthread_mutex_unlock (pthread_mutex_t * mutex);
-	ft_printf("sleep\n");
+	pthread_mutex_lock (pthread_mutex_t * mutex);
+	ft_printf("%d %s thinks like a genius⚡\n", timestamp_in_m, philosopher[i]);
+	pthread_mutex_unlock (pthread_mutex_t * mutex);
+	pthread_mutex_lock (pthread_mutex_t * mutex);
+	ft_printf("%d %s sleep in yours dreams🌕\n", timestamp_in_m, philosopher[i]);
+	pthread_mutex_unlock (pthread_mutex_t * mutex);
+	pthread_mutex_lock (pthread_mutex_t * mutex);
+	ft_printf("%d %s just died🗿\n", timestamp_in_m, philosopher[i]);
 }
 
 static void 	init_values(char *argv)
 {
-	philos = ft_atoi(argv[1]);
-	t_die = argv[2];
-	t_eat = argv[3];
-	t_sleep = argv[4];
-	[t_eat_all]
+	unsigned long current;
+	
+	current = time();
+	timestamp_in_m = (gettimeofday - current);
+	
+	number_of_philosopher = ft_atoi(argv[1]);
+	time_to_die = argv[2];
+	time_to_eat = argv[3];
+	time_to_sleep = argv[4];
+	number_of_times_each_philosopher_must_eat = 0;
+	if (argv[5])
+		number_of_times_each_philosopher_must_eat = argv[5];
+	
 }
 
 int main(int argc, char **argv)
 {
-	int n;
 	int i;
 
 	if (argc != 6)
 		return (ft_printf("Erreur : nombre d'arguments incorrect"));
 	init_values(argv);
-	n = ft_atoi(argv[1]);
 	i = 0;
-	while(i <= n)
+	while(i <= number_of_philosopher)
 	{
 		i++;
 		pthread_create(&philosopher[i], NULL, live_likeem, NULL);
--- time.c
+++ time.c
@@ -3,24 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   time.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: manon <manon@student.42.fr>                +#+  +:+       +#+        */
+/*   By: mlemerci <mlemerci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/13 15:35:15 by manon             #+#    #+#             */
-/*   Updated: 2025/05/13 15:53:19 by manon            ###   ########.fr       */
+/*   Updated: 2025/05/17 19:30:42 by mlemerci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
-unsigned long	get_time(void)
+unsigned long	time(void)
 {
 	struct timeval	tv;
 	unsigned long	last_update;
 	unsigned long	current;
 
 	gettimeofday (&tv, NULL);
-	return ((tv.tv_sec * 1000) + (tv.tv_usec / 1000));
-	current = get_time();
+		return ((tv.tv_sec * 1000) + (tv.tv_usec / 1000));
+	current = time();
 	//|| current = (tv.tv_sec * 1000) + (tv.tv_usec / 1000)
 
 	if (current - last_update >= 500) //500 = demi seconde
